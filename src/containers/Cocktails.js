483a4e068d4d88044feec8d718507b3a9b547a18
joeljdv <joeldiazjdv@gmail.com>
2021-05-04 11:43:21 -0400

    fetch cocktails and set state

--- src/containers/Cocktails.js
+++ src/containers/Cocktails.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react'
+import Navigation from '../components/Navigation'
 
+let url = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
 
-let URL = 'www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
 export default class Cocktails extends Component {
 
     state={
         cocktails:[]
     }
 
-    componentDidMount() {
-        fetch(URL) 
+    componentDidMount(){
+        fetch(url)
         .then(res => res.json())
         .then(data => {
             console.log(data)
+            this.setState({
+                cocktails: data
+            })
         })
     }
+
+  
 
     render() {
         return (
             <div>
-                
+                <Navigation/>
+                <h1>Cocktail list</h1>
             </div>
         )
     }
