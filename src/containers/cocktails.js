a975d139d540b22b190dbfef1652164b1b095fd9
joeljdv <joeldiazjdv@gmail.com>
2021-05-04 11:15:29 -0400

    create cocktails file

---
+++ src/containers/cocktails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class cocktails extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
