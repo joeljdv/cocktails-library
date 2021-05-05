5c515f4235a9acf526ebb6835a9047d258ab0052
joeljdv <joeldiazjdv@gmail.com>
2021-05-05 12:58:25 -0400

    create MyRecipes file

---
+++ src/containers/MyRecipes.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navigation from '../components/Navigation'
+
+export default class MyRecipes extends Component {
+    render() {
+        return (
+            <div>
+                <Navigation/>
+            </div>
+        )
+    }
+}
