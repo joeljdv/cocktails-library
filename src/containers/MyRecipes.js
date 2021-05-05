abd96b8422305d799d5ef1f7d93f2954c77ee952
joeljdv <joeldiazjdv@gmail.com>
2021-05-05 13:48:12 -0400

    add fetch to internal api

--- src/containers/MyRecipes.js
+++ src/containers/MyRecipes.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react'
 import Navigation from '../components/Navigation'
 
 export default class MyRecipes extends Component {
+
+    state = {
+        myRecipes:[]
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/drinks')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            this.setState({
+                myRecipes:data
+            })
+        })
+    }
+
     render() {
         return (
             <div>
